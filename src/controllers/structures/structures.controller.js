65b1f2e07b775ac45468600ac74635077db89954
Robot Jiang <hellolaojiang@gmail.com>
2018-02-02 11:24:30 +0800

    实现 structures API,参见：https://github.com/Orientsoft/prophet-server/issues/10

--- src/controllers/structures/structures.controller.js
+++ src/controllers/structures/structures.controller.js
@@ -1,6 +1,10 @@
 import _ from 'lodash';
+import * as CONSTS from '../../consts';
 import { logger } from '../../lib/logger';
+import { ObjectID, MongoClient } from 'mongodb';
 import errors from '../../lib/errors';
+
+import config from '../../config';
 
 
 
@@ -19,22 +23,7 @@ const treeData = [{
           name: '业务日志',
           level: 2,
           hosts: [],
-          children: [
-            {
-              name: 'tploader',
-              level: 3,
-              children: [{
-                name: '192.168.1.100',
-                level: 4
-              },{
-                name: '192.168.1.101',
-                level: 4
-              },{
-                name: '192.168.1.101',
-                level: 4
-              }]
-            }
-          ]
+          children: []
         },
       ],
     }, {
@@ -151,20 +140,115 @@ const treeData = [{
 
 const metaTreeData = treeData[0]
 
+// rs or mongos connection should work directly with url
+// https://docs.mongodb.com/manual/reference/connection-string/
+const connect = MongoClient.connect(config.mongoURL);
+
+/**
+ * 创建结构
+ * @param {*} req 
+ * @param {*} res 
+ */
+export async function create(req, res) {
+  try {
+      const connection = await connect;
+      const result = await connection.db('prophet-server').collection(CONSTS.STRUCTURE_COLLECTION).insertOne(req.body);
+      if (result.insertedCount === 1) {
+          return res.status(200).json(result.ops[0]);
+      }
+      
+      return res.status(500).send(JSON.stringify(errors.DATA_NOT_INSERTED));
+  }
+  catch(err) {
+      logger.error(`Failed to create structure. ${err.toString()}`);
+      return res.status(500).send(err.toString());
+  }
+}
+
 /**
  * 获取结构列表
  * @param {*} req 
  * @param {*} res 
  */
 export async function structrueList(req, res) {
-  try {
-   
-    res.status(200).json(treeData)
+  const query = _.pickBy({}, _.identity);
 
+  try {
+      const connection = await connect;
+      const structureList = await connection.db('prophet-server').collection(CONSTS.STRUCTURE_COLLECTION).find(query).toArray();
+      return res.status(200).json(structureList);
   }
   catch(err) {
-    logger.error(`Failed to fetch structures info. ${err.toString()}`);
-    return res.status(500).send(err.toString());
+      logger.error(`Failed to find structures. ${err.toString()}`);
+      return res.status(500).send(err.toString());
+  }
+}
+/**
+ * 根据ID获取单个结构
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ * @param {*} id 
+ */
+export async function getStructureById(req, res, next, id) {
+  try {
+      const connection = await connect;
+      const data = await connection.db('prophet-server').collection(CONSTS.STRUCTURE_COLLECTION).findOne({ _id: new ObjectID(id) })
+      if (data !== undefined && data != null) {
+          req.data = data;
+          return next();
+      }
+
+      return res.status(400).send(JSON.stringify(errors.DATA_NOT_FOUND));
+  }
+  catch(err) {
+      logger.error(`Failed to find structure. ${err.toString()}`);
+      return res.status(500).send(err.toString());
+  }
+}
+
+export function read(req, res) {
+  return res.status(200).json(req.data);
+}
+/**
+ * 根据ID更新结构
+ * @param {*} req 
+ * @param {*} res 
+ */
+export async function update(req, res) {
+  try {
+      const { body } = req;
+      const connection = await connect;
+      const result = await connection.db('prophet-server').collection(CONSTS.STRUCTURE_COLLECTION).replaceOne({_id: req.data._id}, body);
+      if (result.modifiedCount === 1 || result.matchedCount === 1) {
+          return res.status(200).json(result.ops[0]);
+      }
+
+      return res.status(400).end(JSON.stringify(errors.DATA_NOT_FOUND));
+  }
+  catch(err) {
+      logger.error(`Failed to update structure. ${err.toString()}`);
+      return res.status(500).send(err.toString());
+  }
+}
+/**
+ * 根据ID删除结构
+ * @param {*} req 
+ * @param {*} res 
+ */
+export async function remove(req, res) {
+  try {
+      const connection = await connect;
+      const result = await connection.db('prophet-server').collection(CONSTS.STRUCTURE_COLLECTION).deleteOne({_id: req.data._id});
+      if (result.deletedCount === 1) {
+          return res.status(200).end();
+      }
+  
+      return res.status(400).end(JSON.stringify(errors.DATA_NOT_FOUND));
+  }
+  catch(err) {
+      logger.error(`Failed to delete structure. ${err.toString()}`);
+      return res.status(500).send(err.toString());
   }
 }
 
--- src/routers/structures.js
+++ src/routers/structures.js
@@ -6,6 +6,14 @@ const router = express.Router();
 // structures
 router.route('/structures')
     .get(StructuresCtrl.structrueList)
+    .post(StructuresCtrl.create)
+
+router.route('/structures/:structureId')
+    .get(StructuresCtrl.read)
+    .put(StructuresCtrl.update)
+    .delete(StructuresCtrl.remove)
+
+router.param('structureId', StructuresCtrl.getStructureById)
 
 router.route('/meta-structure')
     .get(StructuresCtrl.structrueMetaData)
