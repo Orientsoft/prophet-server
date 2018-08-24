f7d0aa8e8c706a23c39b2167608569a38be764f7
pixcai <pixcai@163.com>
2018-08-24 11:06:12 +0800

    Add user

---
+++ src/models/user.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+
+export const RoleType = {
+    ADMIN: 1,
+    DEFAULT: 2,
+    DEVELOPER: 4,
+};
+
+const userSchema = new mongoose.Schema({
+    username: { type: String },
+    password: { type: String },
+    createdAt: { type: Date, default: Date.now },
+    role: { type: Number, default: RoleType.DEFAULT },
+}, { collection: 'users' });
+
+userSchema.methods.validatePassword = function validatePassword(password) {
+    return this.get('password') === password;
+};
+
+export default mongoose.model('User', userSchema);
